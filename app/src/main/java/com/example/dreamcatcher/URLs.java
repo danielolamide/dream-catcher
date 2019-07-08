611df87d0bf702f6069237274916b1933e9c2d10
Afandi Indiatsi <schatze@Afandis-MacBook-Pro.local>
2019-07-09 00:25:38 +0300

    Schatze's changes

--- app/src/main/java/com/example/dreamcatcher/URLs.java
+++ app/src/main/java/com/example/dreamcatcher/URLs.java
@@ -2,7 +2,7 @@ package com.example.dreamcatcher;
 
 public class URLs {
     // stores all the available URLs
-    private static final String ROOT_URL = "http://127.0.0.1:8000/api/";
+    private static final String ROOT_URL = "http://[::]:8000/";
 
     static final String URL_REGISTER = ROOT_URL + "register";
     static final String URL_LOGIN = ROOT_URL + "login";
