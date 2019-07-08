ec09a7514cff2b1513ffdff269ac9fb3a007cc9e
Daniel Olamide <dannyolamide@gmail.com>
2019-07-08 23:20:09 +0300

    NewsCard UI

--- app/src/main/java/com/example/dreamcatcher/TaskModel.java
+++ app/src/main/java/com/example/dreamcatcher/TaskModel.java
@@ -6,7 +6,8 @@ public class TaskModel {
     public String taskTimeLeft;
 
 
-    public TaskModel(String taskName, String taskTimeLeft) {
+    public TaskModel( String taskName, String taskTimeLeft) {
+        this.activeTasks = activeTasks;
         this.taskName = taskName;
         this.taskTimeLeft = taskTimeLeft;
     }
